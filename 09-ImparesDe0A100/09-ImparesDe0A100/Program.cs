3ba3b678dd6f1e8a996aaf53843ef777d705d842
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 20:25:29 -0300

    Add files via upload

---
+++ 09-ImparesDe0A100/09-ImparesDe0A100/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _09_ImparesDe0A100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, a;
+            a = 0;
+            for (i = 0; i <= 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    a = a + 1;
+                    Console.WriteLine("Los numeros impares son: " + i);
+                }
+            }
+            Console.WriteLine("La cantidad de numeros impares es de: " + a);
+            Console.ReadKey();
+        }
+    }
+}
